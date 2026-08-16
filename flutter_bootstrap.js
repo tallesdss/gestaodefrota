d69da4cb157681f60155621fc280f288bd791294
tallesdss <tallesdss@users.noreply.github.com>
2026-08-16 05:23:29 +0000

    deploy: ea563df2b1448275f03ed8ea51946900bd445be9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1233033217" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1773653125" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
